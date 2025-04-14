74a36a05fc424d990c5bdb1be5741a739cb04f89
Victor <victor.orlu@bdic.ng>
2025-04-14 10:53:01 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
     images: {
         formats: ['image/webp'],
     },
-    output: 'standalone', // Lean deployment
+    output: 'standalone',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
