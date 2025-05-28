de457c34acc6f9c2154549cd4d9fefa9b084d6dc
Victor <victor.orlu@bdic.ng>
2025-05-28 16:46:04 +0100

    commit

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -71,7 +71,7 @@ const ChatList: React.FC<ChatListProps> = ({ currentUser, onSelectChat }) => {
                         onClick={() => onSelectChat(user)}
                     >
                         <div className="flex justify-between items-center">
-                            <h3 className="font-medium">{user.name}</h3>
+                            <h3 className="font-medium">{user.firstName}</h3>
                             {user.unreadCount && user.unreadCount > 0 && (
                                 <span className="bg-blue-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                   {user.unreadCount}
